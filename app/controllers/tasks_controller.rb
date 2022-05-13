7d3495333391f009f8b9433f8e403a9ae0aa168a
Supermalonz <kavekhunsri@gmail.com>
2022-05-13 13:35:49 +0700

    update CRUD for to-do-app

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,51 @@
 # frozen_string_literal: true
-
 class TasksController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :set_task, only: %i[show edit update destroy]
 
-  def index; end
+  def index
+    @tasks = current_user.tasks
+    @true_tasks = @tasks.where(status: true)
+    @false_tasks = @tasks.where(status: false)
+  end
+
   def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :body, :status)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :status, inclusion: [true, false]
 
+  belongs_to :user
 end
